52523fb1b0c9f4a9958d506760386a55fd191818
tundera <tunderadev@gmail.com>
2021-02-17 12:53:08 -0700

    Init commit

--- slices/PlayerProfile/index.js
+++ slices/PlayerProfile/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { array, shape } from 'prop-types';
-import { RichText } from 'prismic-reactjs';
+import React from "react";
+import { array, shape } from "prop-types";
+import { RichText } from "prismic-reactjs";
 
 const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
+  maxWidth: "600px",
+  margin: "4em auto",
+  textAlign: "center",
 };
 
 const h2 = {
-  color: '#8592e0',
+  color: "#8592e0",
 };
 
-const MySlice = ({ slice }) => (
+const PlayerProfile = ({ slice }) => (
   <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
+    {slice.primary.title ? (
+      <RichText render={slice.primary.title} />
+    ) : (
+      <h2 style={h2}>Template slice, update me!</h2>
+    )}
+    {slice.primary.description ? (
+      <RichText render={slice.primary.description} />
+    ) : (
+      <p>start by editing this slice from inside the SliceMachine builder!</p>
+    )}
   </section>
 );
 
-MySlice.propTypes = {
+PlayerProfile.propTypes = {
   slice: shape({
     primary: shape({
       title: array.isRequired,
@@ -35,4 +35,4 @@ MySlice.propTypes = {
   }).isRequired,
 };
 
-export default MySlice;
+export default PlayerProfile;
